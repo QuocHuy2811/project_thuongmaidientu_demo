4d33cd1c7ee1405c0626d04c2a53a974909e6e50
QuocHuy2811 <0306231201@caothang.edu.vn>
2025-10-20 10:37:05 +0700

    Update noi dung trang admin

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -6,5 +6,5 @@
 @section('js')
 
 @section('content')
-    <p style="font-size: 50px; color: grey;" class="text-center">Welcome to Admin Home</p>
+    <p style="font-size: 50px; color: grey;" class="text-center">Đây là trang Admin</p>
 @endsection
